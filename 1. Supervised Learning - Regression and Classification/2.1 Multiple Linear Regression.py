c9ad9e8c8341611c210d02ebb95d593a54e70fe4
Hicazi <hicaziolmez6@gmail.com>
2025-07-16 02:30:00 +0300

    finished mlr

---
+++ 1. Supervised Learning - Regression and Classification/2.1 Multiple Linear Regression.py
@@ -0,0 +1,148 @@
+import copy, math
+import numpy as np
+
+# Load our data set
+X_train = np.array([[1.0, 2.0,3.0, 4.0], [2.0, 3.0, 4.0, 5.0]])   #features
+y_train = np.array([300.0, 500.0])   #target value
+
+# data is stored in numpy array/matrix
+print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+print(X_train)
+print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+print(y_train)
+
+#Near Optimal w and b Values for Experimenting
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+
+def predict_single_loop(x,w,b):
+    """
+    single predict using linear regression
+    
+    Args:
+      x (ndarray): Shape (n,) example with multiple features
+      w (ndarray): Shape (n,) model parameters    
+      b (scalar):  model parameter     
+      
+    Returns:
+      p (scalar):  prediction
+    """
+    n = x.shape[0]
+    p=0
+    for i in range(n):
+        p_i = x[i] * w[i]
+        p+=p_i
+    p+=b
+
+    return p
+
+# get a row from our training data
+x_vec = X_train[0,:]
+print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+
+# make a prediction
+f_wb = predict_single_loop(x_vec, w_init, b_init)
+print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+
+def compute_cost(x, y, w, b):
+    """
+    compute cost
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      cost (scalar): cost
+    """
+    m = x.shape[0]
+    cost = 0
+    total_cost = 0
+    for i in range(m):
+        f_wb = np.dot(w, x[i])+b
+        cost += (f_wb - y[i])**2
+    total_cost = cost/(2*m)
+
+    return total_cost
+
+# Compute and display cost using our pre-chosen optimal parameters. 
+cost = compute_cost(X_train, y_train, w_init, b_init)
+print(f'Cost at optimal w : {cost}')
+
+def compute_gradient(x, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = x.shape
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+    for i in range(m):
+        f_wb = np.dot(w, x[i])+b
+        for j in range(n):
+            dj_dw[j] += (f_wb -y[i])*x[i,j]
+        dj_db += (f_wb -y[i])
+    dj_dw = (1/m)*dj_dw
+    dj_db = (1/m)*dj_db
+
+    return dj_dw, dj_db
+
+#Compute and display gradient 
+tmp_dj_db, tmp_dj_dw = compute_gradient(X_train, y_train, w_init, b_init)
+print(f'dj_db at initial w,b: {tmp_dj_db}')
+print(f'dj_dw at initial w,b: \n {tmp_dj_dw}')
+
+
+def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+    """
+    Performs batch gradient descent to learn w and b. Updates w and b by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      X (ndarray (m,n))   : Data, m examples with n features
+      y (ndarray (m,))    : target values
+      w_in (ndarray (n,)) : initial model parameters  
+      b_in (scalar)       : initial model parameter
+      cost_function       : function to compute cost
+      gradient_function   : function to compute the gradient
+      alpha (float)       : Learning rate
+      num_iters (int)     : number of iterations to run gradient descent
+      
+    Returns:
+      w (ndarray (n,)) : Updated values of parameters 
+      b (scalar)       : Updated value of parameter 
+      """
+    
+    w = copy.deepcopy(w_in) #avoid modifying global w within function
+    b = b_in
+    dj_dw, dj_db = gradient_function(x,y,w,b)
+    for i in range(num_iters):
+        w += -alpha* dj_dw
+        b += -alpha*dj_db
+    
+    return w,b
+
+
+
+# initialize parameters
+initial_w = np.zeros_like(w_init)
+initial_b = 0.
+# some gradient descent settings
+iterations = 1000
+alpha = 5.0e-7
+# run gradient descent 
+w_final, b_final= gradient_descent(X_train, y_train, initial_w, initial_b, compute_cost, compute_gradient,alpha, iterations)
+print(f"b,w found by gradient descent: {b_final:0.2f},{w_final} ")
+m,_ = X_train.shape
+for i in range(m):
+    print(f"prediction: {np.dot(X_train[i], w_final) + b_final:0.2f}, target value: {y_train[i]}")
